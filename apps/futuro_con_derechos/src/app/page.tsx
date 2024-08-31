68c250e8f6378a12a114cc41312eb6e3a5ee707b
RamonAntonioNaranjoSarmient <a00836530@tec.mx>
2024-08-31 17:24:18 -0600

    Cambio entre vistas de fiscalia y periodicos

--- apps/futuro_con_derechos/src/app/page.tsx
+++ apps/futuro_con_derechos/src/app/page.tsx
@@ -1,17 +1,30 @@
+'use client'
+
 import TopBar from '@/components/top-bar.tsx';
 import dynamic from 'next/dynamic';
-import {Paper} from 'geostats-ui';
+import {Button, Paper} from 'geostats-ui';
+import { useState } from 'react';
 
 const MainMap = dynamic(() => import('@/components/main-map.tsx'), {
 	ssr: false,
 });
 
+
 export default function Home() {
+
+	const [showFiscalia, setShowFiscalia] = useState(false); 
+
 	return (
 		<main className='h-screen w-screen overflow-hidden bg-stone-950 text-stone-300'>
 			<TopBar />
-			<Paper className='absolute right-8 top-20'>Hola!</Paper>
-			<MainMap className='h-full' />
+			<Paper className='absolute right-8 top-20 z-50'>
+
+				<Button onPress={() => {setShowFiscalia(prevState => !prevState)}} >
+					Cambiar de vista
+				</Button>
+				
+			</Paper>
+			<MainMap className='h-full' showFiscalia={showFiscalia} />
 		</main>
 	);
 }
--- apps/futuro_con_derechos/src/components/main-map.tsx
+++ apps/futuro_con_derechos/src/components/main-map.tsx
@@ -1,29 +1,129 @@
 "use client"
 import dynamic from 'next/dynamic'
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useEffect, useRef } from 'react';
+import MapboxGl, {Map} from 'mapbox-gl';
+import { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl'; 
 
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RvY2s0NCIsImEiOiJjbHE0amx3aXEwODQyMmlsb3RnNHk0MDN1In0.qWALc9kC_uuNNBucnTeauw'; 
- 
-export  default function MapScreen(){ 
 
 
+export type MainMapProps = {
+
+	readonly className ? : string
+	readonly showFiscalia : boolean
+}
+
+
+export  default function MapScreen(props: MainMapProps){ 
+
+	const {className, showFiscalia} = props
     
     const mapContainerRef = useRef<HTMLDivElement | null>(null); 
- 
+	
+
+	const [isLoaded, setIsLoaded] = useState(false);
+
+
+	const mapRef = useRef<Map>()
+
+
+	useEffect( ()=> {
+
+		if(!mapRef.current) return
+
+		if(showFiscalia)
+		{
+
+			mapRef.current.addLayer({
+				'id': 'feminicidios-fiscalia',
+				'type': 'circle',
+				
+				'source' : 'feminicidios-fiscalia',
+				'source-layer' : 'Feminicidios_de_Fiscalia_cent-3rdfpy',
+				//'filter': '',
+				//'layout':   ,
+				'maxzoom': 20,
+				//'metadata'   ,
+				'minzoom': 7 ,
+				'paint': {
+	
+					
+					//'fill' : "#000000",
+					'circle-radius': [
+					"interpolate",
+					["linear"],
+					["get", "NUMPOINTS"],
+					1,
+					5,
+					15,
+					20
+					],
+					'circle-color': '#ec5151',  
+					'circle-blur' : 1  
+					
+				},
+					//'slot':  ,
+	
+			});	
+			return () => {
+				mapRef.current?.removeLayer("feminicidios-fiscalia")
+			}
+		}
+
+		mapRef.current.addLayer({
+			'id': 'feminicidios-periodicos',
+			'type': 'circle',
+			
+			'source' : 'feminicidios-periodicos',
+			'source-layer' : 'Feminicidios_de_Periodicos_ce-1wv0e9',
+			//'filter': '',
+			//'layout':   ,
+			'maxzoom': 20,
+			//'metadata'   ,
+			'minzoom': 7 ,
+			'paint': {
+
+				
+			  //'fill' : "#000000",
+			  'circle-radius': [
+				"interpolate",
+				["linear"],
+				["get", "NUMPOINTS"],
+				1,
+				5,
+				15,
+				20
+			  ],
+			  'circle-color': '#ec5151',  
+			  'circle-blur' : 1  
+			  
+			},
+			 //'slot':  ,
+
+		});
+
+		return () => {
+			mapRef.current?.removeLayer("feminicidios-periodicos")
+		}
+
+
+
+	}, [showFiscalia, isLoaded])
+
     useEffect(() => { 
         if (mapContainerRef.current) { 
 
-            const map = new mapboxgl.Map({ 
+            mapRef.current = new mapboxgl.Map({ 
                 container: mapContainerRef.current, 
                 style: 'mapbox://styles/stock44/clwwmpmk7003501nm1y6eh0q4', 
                 center: [-100.31, 25], // starting position [lng, lat] 
                 zoom: 8, // starting zoom 
             }); 
             
-			
+			const map = mapRef.current
+
             map.on('load', () => {
                 // Add a source
 
@@ -32,8 +132,27 @@ export  default function MapScreen(){
                   type: 'vector',
                   url: 'mapbox://stock44.1sgbxxpi'
                 });
+
+				//Area sin cubrimiento
+				map.addSource('area-sin-cubrimiento-de-sitio', {
+					type: 'vector',
+					url: 'mapbox://stock44.2hei66sb'
+				  });
+				
+				map.addSource('feminicidios-periodicos', {
+				type: 'vector',
+				url: 'mapbox://stock44.19vijq6m'
+				});
+
+				map.addSource('feminicidios-fiscalia', {
+					type: 'vector',
+					url: 'mapbox://stock44.2k4nz34n'
+					});
 				
 				
+				setIsLoaded(true);
+
+
                 // Add a layer
                 map.addLayer({
                   'id': 'rezago',
@@ -67,12 +186,7 @@ export  default function MapScreen(){
 
                 });
 				
-				//Area sin cubrimiento
-				map.addSource('area-sin-cubrimiento-de-sitio', {
-					type: 'vector',
-					url: 'mapbox://stock44.2hei66sb'
-				  });
-
+				
                 map.addLayer({
 					'id': 'area-sin-cubrimiento-de-sitio',
 					'type': 'fill',
@@ -95,83 +209,6 @@ export  default function MapScreen(){
 					 //'slot':  ,
   
 				  });
-
-				
-				map.addSource('feminicidios-periodicos', {
-				type: 'vector',
-				url: 'mapbox://stock44.19vijq6m'
-				});
-
-                map.addLayer({
-					'id': 'feminicidios-periodicos',
-					'type': 'circle',
-					
-					'source' : 'feminicidios-periodicos',
-					'source-layer' : 'Feminicidios_de_Periodicos_ce-1wv0e9',
-					//'filter': '',
-					//'layout':   ,
-					'maxzoom': 20,
-					//'metadata'   ,
-					'minzoom': 7 ,
-					'paint': {
-
-						
-					  //'fill' : "#000000",
-					  'circle-radius': [
-						"interpolate",
-						["linear"],
-						["get", "NUMPOINTS"],
-						1,
-						5,
-						15,
-						20
-					  ],
-					  'circle-color': '#ec5151',  
-					  'circle-blur' : 1  
-					  
-					},
-					 //'slot':  ,
-  
-				});
-
-
-				map.addSource('feminicidios-fiscalia', {
-					type: 'vector',
-					url: 'mapbox://stock44.2k4nz34n'
-					});
-	
-					map.addLayer({
-						'id': 'feminicidios-fiscalia',
-						'type': 'circle',
-						
-						'source' : 'feminicidios-fiscalia',
-						'source-layer' : 'Feminicidios_de_Fiscalia_cent-3rdfpy',
-						//'filter': '',
-						//'layout':   ,
-						'maxzoom': 20,
-						//'metadata'   ,
-						'minzoom': 7 ,
-						'paint': {
-	
-							
-						  //'fill' : "#000000",
-						  'circle-radius': [
-							"interpolate",
-							["linear"],
-							["get", "NUMPOINTS"],
-							1,
-							5,
-							15,
-							20
-						  ],
-						  'circle-color': '#ec5151',  
-						  'circle-blur' : 1  
-						  
-						},
-						 //'slot':  ,
-	  
-					});
-
 				
 
               });
@@ -188,5 +225,5 @@ export  default function MapScreen(){
             return () => map.remove(); 
         } 
     }, []); 
-    return <div ref={mapContainerRef} style={{ width: '100%', height: '400px' }} />; 
+    return <div ref={mapContainerRef} className={className} />; 
 }; 
